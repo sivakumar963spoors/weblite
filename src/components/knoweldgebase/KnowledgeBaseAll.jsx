5f0c380c1ca157bb8cf839d844b42eb117fd0486
SUKESHINI <vijaya.vemana@spoors.in>
2024-10-25 17:52:58 +0530

    resolved issue

---
+++ src/components/knoweldgebase/KnowledgeBaseAll.jsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const KnowledgeBaseAll = () => {
+  return (
+    <div>
+      <Box sx={{mt:10}}>
+<Typography>knowledebase all</Typography>
+      </Box>
+    </div>
+  )
+}
+
+export default KnowledgeBaseAll
